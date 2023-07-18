53291202663cb988cfe5b2185737d94c78eb0768
MarquessMateus <tsjesus21@outlook.com>
2023-07-18 16:24:19 -0300

    Adicionando Quantificadores e Intervalos

--- extranindo_cep.py
+++ extranindo_cep.py
@@ -1,10 +1,10 @@
-endereco = 'Rua das flores 72, apartamento 1002, Laranjeiras, Rio de janeiro, RJ, 23440-120'
+endereco = 'Rua das flores 72, apartamento 1002, Laranjeiras, Rio de janeiro, RJ, 23440120'
 
 import re  #Expressões Regulares -- RegEX
 
 # 5 digitos = hifen (opconal) + 3 digitos
 
-padrao = re.compile("[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]")
+padrao = re.compile("[0-9]{5}[-]{0,1}[0-9]{3}")
 busca = padrao.search(endereco)
 
 if busca:
